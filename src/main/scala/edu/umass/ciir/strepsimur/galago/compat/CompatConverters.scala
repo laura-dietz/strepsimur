44e2d658af4cf6f4fbb68490b4a26bf1234219f6
Laura Dietz <dietz@cs.umass.edu>
2014-02-28 14:53:51 -0500

    Recycling EntityExpanderBoltHistory with hookups to the strepsi/mur packages
    Fixing some bugs in Wikititle - freebase Id confusion
    
    renaming toplevel package "du" to "edu"

--- src/main/scala/edu/umass/ciir/strepsimur/galago/compat/CompatConverters.scala
+++ src/main/scala/edu/umass/ciir/strepsimur/galago/compat/CompatConverters.scala
@@ -3,6 +3,7 @@ package edu.umass.ciir.strepsimur.galago.compat
 import edu.umass.ciir.strepsi.galagocompat.GalagoTag
 import org.lemurproject.galago.core.parse.{Document, Tag}
 import scala.collection.JavaConversions._
+import edu.umass.ciir.strepsi.ScoredDocument
 
 /**
  * User: dietz
@@ -16,5 +17,16 @@ object CompatConverters {
 
   def doc2Tag(document:Document):Seq[GalagoTag] = {
     document.tags.map(strepsiTagToGalagoTag(_, document.name))
+  }
+
+  def doc2TermTag(document:Document):(Seq[String],Seq[GalagoTag]) = {
+    (document.terms, document.tags.map(strepsiTagToGalagoTag(_, document.name)))
+  }
+
+  def gDoc2sDoc(scoredDocument:org.lemurproject.galago.core.retrieval.ScoredDocument):ScoredDocument = {
+    new ScoredDocument(scoredDocument.documentName, scoredDocument.rank, scoredDocument.score)
+  }
+  def gDocSeq2sDocSeq(gDocSeq:Seq[org.lemurproject.galago.core.retrieval.ScoredDocument]):Seq[ScoredDocument] = {
+    for (doc <- gDocSeq) yield CompatConverters.gDoc2sDoc(doc)
   }
 }
